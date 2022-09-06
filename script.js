0193e60209f46a8b718dd31ba3e453d031f3a041
Erinne Grant <erinne.grant@gmail.com>
2022-09-05 17:02:23 -0700

    optimized form logic

--- script.js
+++ script.js
@@ -24,18 +24,15 @@ function showSuccess(input) {
 }
 
 //#### Check email is valid
-// javascript email regex --> stack overflow
-function isValidEmail(email) {
+// javascript email regex sourced from stack overflow
+
+function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());  
-    
-    
-    
-    /*if (re.test(input.value.trim())) {
+    if (re.test(input.value.trim())) {
           showSuccess(input);
         } else {
           showError(input, 'Email is not valid');
-        } */
+        } 
 }
 
 
@@ -60,7 +57,22 @@ function checkRequired(inputArr) {
 
 //#### Check input length
 
+function checkLength(input, min, max) {
+    if(input.value.length < min){
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
 
+//#### Passwords match
+function checkPasswordsMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+      showError(input2, 'Passwords do not match');
+    }
+  }
 
 
 //#### Get fieldname
@@ -77,6 +89,8 @@ form.addEventListener('submit', function(e){
  checkRequired([username, email, password, password2]);
  checkLength(username, 3, 15) //(input, min, max)
  checkLength(password, 6, 25);
+ checkEmail(email)
+ checkPasswordsMatch(password, password2);
 })
 
 
